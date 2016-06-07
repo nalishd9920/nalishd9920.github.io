323de9e8db8b22e650988fbeba0f1a05d9a152e2
nalishd9920 <nalishd9920@kippnyccp.org>
2016-06-07 10:55:39 -0400

    Update application.js

--- guess/application.js
+++ guess/application.js
@@ -1,7 +1,16 @@
 
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
+	
+	$("#submit").click(function(){
+		//find out the guess
+		var guess = $("input[");
+		if guess == random_num{
+			$("#response").html("<p>Good guess!</p>");
+		}
+		
+	});
 	<button id="submit" onClick="reply_click()"></button>
-	<button id="2" onClick="reply_click()"></button>
+	<button id="clear" onClick="reply_click()"></button>
 
 });
