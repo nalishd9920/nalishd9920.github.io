03929938943e5046fda02730bb3663da665a5aa7
nalishd9920 <nalishd9920@kippnyccp.org>
2016-06-09 10:58:30 -0400

    Update application.js

--- application.js
+++ application.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#submit").click(function(){
     var name=$("input[name=firstname]").val();
     var comment=$("input[name=comment]").val();
-    alert(comment);
+    $("#Box").append("<b><p>"+name+"</p></b><br>");
+    $("#Box").append("<p>"+comment+"</p><br>");
   })
 })
